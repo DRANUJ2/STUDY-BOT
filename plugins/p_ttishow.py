ce07cee9c2ed8899e88924e173d7fef85ce53f72
DOCTORXANUJ1 <anuj848671@gmail.com>
2025-08-17 16:18:51 +0530

    update

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from info import ADMINS, MULTIPLE_DB, LOG_CHANNEL, OWNER_LNK, MELCOW_PHOTO
 from database.users_chats_db import db, db2
-from database.ia_filterdb import Media, Media2, db as db_stats, db2 as db2_stats
+from database.ia_filterdb import Media, Media2, db as ia_db, db2 as ia_db2
 from utils import get_size, temp, get_settings, get_readable_time
 from Script import script
 from pyrogram.errors import ChatAdminRequired
